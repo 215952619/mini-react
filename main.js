c2d48910d242eacd61cea12c3b8ec122a3bb53ec
lyyC2 <215952619@qq.com>
2024-03-23 23:43:49 +0800

    feat: 拆分出创建节点逻辑

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 const root = document.getElementById('root');
 
-const appDom = {
-  type: 'div',
-  props: {
-    id: 'app',
-  },
+const createTextNode = (text) => {
+  return {
+    type: 'TEXT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
 };
 
-const textNodeDom = {
-  type: 'TEXT',
-  props: {
-    textContent: 'app',
-    children: [],
-  },
+const createElementNode = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  };
 };
 
-const app = document.createElement(appDom.type);
-app.id = appDom.props.id;
+const Text = createTextNode('app');
+const App = createElementNode('div', { id: 'app' }, Text);
+
+const app = document.createElement(App.type);
+app.id = App.props.id;
 
 const textNode = document.createTextNode('');
-textNode.textContent = textNodeDom.props.textContent;
+textNode.textContent = Text.props.nodeValue;
 
 app.appendChild(textNode);
 root.appendChild(app);
